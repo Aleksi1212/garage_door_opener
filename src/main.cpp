80bc64522f3d4b84e2c2dbeba868b5ed11cbe886
Aleksi Noro <aleksinoro@gmail.com>
2025-09-19 18:07:46 +0300

    the clanker is blinking

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <pico/stdlib.h>
+#include <hardware/gpio.h>
+
+using namespace std;
+
+#define LED 20
+
+int main()
+{
+    stdio_init_all();
+
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+
+    while (true)
+    {
+        gpio_put(LED, true);
+        sleep_ms(1000);
+        gpio_put(LED, false);
+        sleep_ms(1000);
+    }
+
+    return 0;
+}
