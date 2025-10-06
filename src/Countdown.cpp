839be435c7647ed2a9cb72c27b65a5b16269179c
Aleksi Noro <aleksinoro@gmail.com>
2025-10-06 15:44:16 +0300

    connect to MQTT server and subsrcribe to a topic

---
+++ src/Countdown.cpp
@@ -0,0 +1,30 @@
+#include <Countdown.hpp>
+
+Countdown::Countdown() {
+    target_time = make_timeout_time_ms(0);
+}
+
+Countdown::Countdown(int ms) {
+    target_time = make_timeout_time_ms(ms);
+}
+
+
+bool Countdown::expired() {
+    return time_reached(target_time);
+}
+
+
+void Countdown::countdown_ms(int ms) {
+    target_time = make_timeout_time_ms(ms);
+}
+
+
+void Countdown::countdown(int seconds) {
+    target_time = make_timeout_time_ms(seconds * 1000);
+}
+
+
+int Countdown::left_ms() {
+    return static_cast<int>(absolute_time_diff_us(get_absolute_time(), target_time) / 1000);
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 #include <hardware.hpp>
 #include <irq_callback.hpp>
 
+#include <Countdown.hpp>
+#include <IPStack.hpp>
+#include "MQTTClient/src/MQTTClient.h"
+#include "network_info.h"
+
 using namespace std;
 
 void irq_callback(uint gpio, uint32_t event_mask)
@@ -18,28 +23,103 @@ void irq_callback(uint gpio, uint32_t event_mask)
     }
 }
 
+void messageArrived(MQTT::MessageData &md) {
+    MQTT::Message &message = md.message;
+
+    printf("Message arrived: qos %d, retained %d, dup %d, packetid %d\n",
+           message.qos, message.retained, message.dup, message.id);
+    printf("Payload %s\n", (char *) message.payload);
+}
+
+
 int main()
 {
     stdio_init_all();
+
+    cout << "\nBOOT\n" << endl;
 
     auto ps_ptr = make_shared<ProgramState>();
 
     GarageDoor garage_door(ps_ptr);
 
+    GPIOPin led1(LED_1, -1, false, false);
+    GPIOPin led2(LED_2, -1, false, false);
+    GPIOPin led3(LED_3, -1, false, false);
+
+    led1.write(true);
+    led2.write(true);
+    led3.write(true);
+
+
+    const char *command_topic = "garage/door/command";
+    const char *response_topic = "garage/door/response";
+    const char *status_topic = "garage/door/status";
+
+    IPStack ipstack(SSID, PW);
+    auto client = MQTT::Client<IPStack, Countdown>(ipstack);
+
+    int rc = ipstack.connect(HOSTNAME, PORT);
+    if (rc != 1) {
+        cout << "rc from TCP connect is " << rc << endl;
+    }
+
+    led1.write(true);
+    led2.write(false);
+    led3.write(false);
+
+    cout << "MQTT connecting..." << endl;
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = (char *)"PicoW-sample";
+
+    rc = client.connect(data);
+    if (rc != 0) {
+        cout << "rc from MQTT connect is " << rc << endl;
+        while (true) {
+            tight_loop_contents();
+
+            led1.write(true);
+            led2.write(true);
+            led3.write(true);
+            sleep_ms(500);
+            led1.write(false);
+            led2.write(false);
+            led3.write(false);
+            sleep_ms(500);
+
+        }
+    }
+    cout << "MQTT connected" << endl;
+    led1.write(false);
+    led2.write(true);
+    led3.write(false);
+
+    rc = client.subscribe(command_topic, MQTT::QOS2, messageArrived);
+    if (rc != 0) {
+        cout << "rc from MQTT subscribe is " << rc << endl;
+    }
+    led1.write(false);
+    led2.write(false);
+    led3.write(true);
+    // cout << "MQTT subscribed to " << command_topic << endl;
+
+
     while (true)
     {
-        auto ps = ps_ptr->read();
-        cout << "Calibrated: " << (int)ps.calibrated << endl;
-        cout << "Door position: " << (int)ps.door_position << endl;
-        cout << "Is open: " << (int)ps.is_open << endl;
-        cout << "Is running: " << (int)ps.is_running << endl;
-        cout << "Steps up: " << (int)ps.steps_up << endl;
-        cout << "Steps down: " << (int)ps.steps_down << "\n\n" << endl;
+        cout << "MQTT subscribed to " << command_topic << endl;
 
-        if (!ps.calibrated) {
-            garage_door.calibrate_motor();
-        }
-        garage_door.reset();
+        // auto ps = ps_ptr->read();
+        // cout << "Calibrated: " << (int)ps.calibrated << endl;
+        // cout << "Door position: " << (int)ps.door_position << endl;
+        // cout << "Is open: " << (int)ps.is_open << endl;
+        // cout << "Is running: " << (int)ps.is_running << endl;
+        // cout << "Steps up: " << (int)ps.steps_up << endl;
+        // cout << "Steps down: " << (int)ps.steps_down << "\n\n" << endl;
+
+        // if (!ps.calibrated) {
+        //     garage_door.calibrate_motor();
+        // }
+        // garage_door.reset();
         sleep_ms(100);
     }
 
